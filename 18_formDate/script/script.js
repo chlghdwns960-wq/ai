c378e9836ca5c8405af268c3ee818289f32b4a55
1 <chlghdwns960@gmail.com>
2026-04-03 09:49:11 +0900

    Add files via upload

--- 18_formDate/script/script.js
+++ 18_formDate/script/script.js
@@ -5,13 +5,17 @@ const msg = document.getElementById("msg");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
     .then((response) => {
       msg.innerHTML = "Message sent successfully";
-      setTimeout(function () {
+      setTimeout(() => {
         msg.innerHTML = "";
       }, 5000);
       form.reset();
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => console.error("Error!", error));
 });
