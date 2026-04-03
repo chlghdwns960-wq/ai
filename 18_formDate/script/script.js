54d6cff9488ee6a184a72d547d21d70416e62b93
1 <chlghdwns960@gmail.com>
2026-04-03 10:03:02 +0900

    Add files via upload

--- 18_formDate/script/script.js
+++ 18_formDate/script/script.js
@@ -1,21 +1,18 @@
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbyJslRG1ED9zLCPiI1bm_SQWLJHwGrF92SkGLsTLuQQ1eQlqcJa-umzGSQk5yPt7ip5/exec.google.com/macros/s/AKfycbyJslRG1ED9zLCPiI1bm_SQWLJHwGrF92SkGLsTLuQQ1eQlqcJa-umzGSQk5yPt7ip5/execadd"; // add your own app script link here
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  fetch(scriptURL, {
-    method: "POST",
-    body: new FormData(form),
-  })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully";
-      setTimeout(() => {
-        msg.innerHTML = "";
-      }, 5000);
-      form.reset();
+<script>
+    const scriptURL = '< add you own link here >' // add your own app script link here
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
     })
-    .catch((error) => console.error("Error!", error));
-});
+  </script>
