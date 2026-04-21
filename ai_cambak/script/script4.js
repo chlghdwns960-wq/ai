9759bd4525997821b20c4a6899b602578ec314a7
1 <chlghdwns960@gmail.com>
2026-04-21 15:13:37 +0900

    Add files via upload

---
+++ ai_cambak/script/script4.js
@@ -0,0 +1,11 @@
+const heroSwiper = new Swiper('.heroSwiper', {
+  loop: true,
+  speed: 550,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  allowTouchMove: true,
+});
